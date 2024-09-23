c5241ff8523a8a6d9902af52ab272e563c05a30d
Tejas Adhikari <tejasadhikari123@gmail.com>
2024-09-22 20:19:41 -0400

    Added CSS to Dashboard and Account

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -5,6 +5,7 @@ import Home from "./Home";
 import Assignments from "./Assignments"
 import AssignmentEditor from "./Assignments/Editor";
 import { FaAlignJustify } from "react-icons/fa";
+import PeopleTable from "./People/Table";
 
 export default function Courses () {
     return (
@@ -29,7 +30,7 @@ export default function Courses () {
                     <Route path="Assignments/:aid" element={<AssignmentEditor />} />                  
                     <Route path="Quizzes" element={<h2>Quizzes</h2>} />
                     <Route path="Grades" element={<h2>Grades</h2>} />
-                    <Route path="People" element={<h2>People</h2>} />
+                    <Route path="People" element={<PeopleTable />} />
                     </Routes>
                 </div>
             </div>     
