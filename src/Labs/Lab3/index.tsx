b75ab322f5d45d825b1392a4a74a2beea901adae
Tejas Adhikari <tejasadhikari123@gmail.com>
2024-10-01 01:09:01 -0400

    a3 initial Commit

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,18 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator"; 
+
 export default function Lab3() {
     return (
-        <div>
+        <div id="wd-lab3" className="container-fluid">
             <h2>Lab3</h2>
+            <VariablesAndConstants/>
+            <VariableTypes />
+            <BooleanVariables />
+            <IfElse />
+            <TernaryOperator />
         </div>
     );
 }
