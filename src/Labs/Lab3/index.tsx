5d125b25fc0ba31a6927a28a4024dbc9743869ff
Tejas Adhikari <tejasadhikari123@gmail.com>
2024-10-02 21:55:35 -0400

    Additions to Lab3 till 2.5

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -19,6 +19,14 @@ import FindIndex from "./FindIndex"
 import FilterFunction from "./FilterFunction"
 import JsonStringify from "./JsonStringify"
 import House from "./House"
+import TodoItem from "./todo/TodoItem"
+import TodoList from "./todo/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
 
 export default function Lab3() {
     return (
@@ -45,6 +53,14 @@ export default function Lab3() {
             <FilterFunction />
             <JsonStringify />
             <House />
+            <TodoItem />
+            <TodoList />
+            <Spreading />
+            <Destructing />
+            <FunctionDestructing />
+            <DestructingImports />
+            <Classes />
+            <Styles />
         </div>
     );
 }
