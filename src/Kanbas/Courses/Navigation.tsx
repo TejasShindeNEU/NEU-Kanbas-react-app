8fd2b39cdeeb510baff5cea9035a0690608c8c79
Tejas Adhikari <tejasadhikari123@gmail.com>
2024-10-05 22:34:35 -0400

    A3 Completed

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -5,8 +5,10 @@ export default function CoursesNavigation() {
   const { pathname } = useLocation();
   const links = ["Home", "Modules", "Piazza", "Zoom", 
     "Assignments", "Quizzes", "Grades", "People"];
+    
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
+
   return (
     <div id="wd-courses-navigation"  className="wd list-group fs-5 rounded-0">
       {links.map((link) => (
