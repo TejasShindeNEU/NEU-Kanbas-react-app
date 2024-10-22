8760b987102ba784e0672bbe3ebd9a21d980395c
Tejas Adhikari <tejasadhikari123@gmail.com>
2024-10-21 22:01:35 -0400

    completed 2.3

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -8,6 +8,7 @@ import StringStateVariables from "./StringStateVariables";
 import DateStateVariable from "./DateStateVariables";
 import ObjectStateVariable from "./ObjectStateVariables";
 import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 
 export default function Lab1() {
     function sayhello() {
@@ -27,6 +28,7 @@ export default function Lab1() {
             <DateStateVariable />
             <ObjectStateVariable />
             <ArrayStateVariable />
+            <ParentStateComponent />
         </div>
     )
 };
